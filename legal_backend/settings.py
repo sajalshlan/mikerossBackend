41ef1582552ff3ff7d8bddbf04453579a2fe9b38
sajal-personal <sharma24sajal@gmail.com>
2024-10-22 01:38:34 +0530

    setting up the scripts to migrate to aws

--- legal_backend/settings.py
+++ legal_backend/settings.py
@@ -44,12 +44,15 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'api',
+    'whitenoise.runserver_nostatic'
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
